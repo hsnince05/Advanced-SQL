5e20da32640172e147e905fed52f1061b4e344cd
Hasan INCE <hsnc0505@gmail.com>
2023-03-08 23:46:58 +0300

    AdvencedSQL Day03 08/03/2023

--- Advenced-SQL-Day01-Day02.sql
+++ Advenced-SQL-Day01-Day02.sql
@@ -300,4 +300,198 @@ begin
 	end if;
 end $$;
 
+--Task 1 : Film tablosundaki film sayısı 10 dan az ise "Film sayısı az" yazdırın, 
+--			10 dan çok ise "Film sayısı yeterli" yazdıralım
+​
+do $$
+declare
+	sayi integer :=0;
+		
+begin
+	SELECT count(*) FROM film  -- 4
+	into sayi; -- sayi=4
+	
+	if (sayi<10) then
+			raise notice 'Filim sayısı az';
+		else
+			raise notice 'Filim sayısı yeterli';
+	end if;
+end $$;
+
+-- Task 2: user_age isminde integer data türünde bir değişken tanımlayıp default olarak bir
+--değer verelim, If yapısı ile girilen değer 18 den büyük ise Access Granted, küçük ise
+--Access Denied yazdıralım
+do $$
+declare
+user_age int :=25;
+begin
+	
+	if(user_age>18)then
+	raise notice 'acces Granted';
+	else 
+	raise notice 'Access Denied';
+	end if;
+end $$
+
+
+
+-- Task 3: a ve b isimli integer türünde 2 değişken tanımlayıp default 
+--	değerlerini verelim, eğer a nın değeri b den büyükse "a , b den büyüktür" 
+--	yazalım, tam tersi durum için "b, a dan büyüktür" yazalım, iki değer 
+--	birbirine eşit ise " a,  b'ye eşittir" yazalım: 
+​
+DO $$
+DECLARE
+	a integer := 10;
+	b integer := 20;
+​
+BEGIN
+​
+	IF a>b THEN
+		RAISE NOTICE 'a, b den büyüktür';
+	END IF;
+	
+	IF a<b THEN
+		RAISE NOTICE 'b, a dan büyüktür';
+	END IF;
+	
+	IF a=b THEN
+		RAISE NOTICE 'a, b ye eşittir';
+	END IF;
+​
+END $$;
+
+-- 2: YOL 
+do $$
+declare
+	a int :=10;
+	b int :=5;
+begin
+	if a>b then
+		raise notice 'a b den büyüktür';
+	elseif a<b then
+		raise notice 'b a dan büyüktür';
+	elseif a=b then
+		raise notice 'a b ye eşittir';
+	end if;
+end $$
+
+-- Task 4 : kullaniciYasi isimli bir değişken oluşturup default değerini verin, girilen
+--yaş 18 den büyükse "Oy kullanabilirsiniz", 18 den küçük ise "Oy kullanamazsınız" yazısını
+--yazalım.
+do $$
+declare
+kullaniciyasi int := 19;
+begin
+	if(kullaniciyasi <18) then
+	raise notice ' oy kullanamazsiniz';
+	else 
+	raise notice ' oy kullanbilirsiniz';
+	end if ;
+end $$
+
+
+--  ************** LOOP *************************************
+
+-- syntax 
+
+LOOP
+	statement;
+END LOOP;
+
+-- loop u sonlandırmak için loopun içine if yapısını kullanabilirz :
+
+LOOP
+	statements;
+	IF condition THEN
+		exit; -- loop dan çıkmamı sağlıyor
+	END IF;
+END LOOP;
+
+-- nested loop 
+
+<<outher>>
+LOOP
+	statements;
+	<<inner>>
+	LOOP
+		.....
+		exit <<inner>>
+		END LOOP;
+END LOOP;
+
+
+-- Task : Fibonacci serisinde, belli bir sıradaki sayıyı ekrana getirelim
+
+-- Fibonacci Serisi : 1,1,2,3,5,8,13,...
+
+DO $$
+DECLARE
+	n integer :=40;
+	counter integer :=0;
+	i integer :=0;
+	j integer :=1;
+	fib integer :=0;	
+​
+BEGIN 
+	if(n<1) then
+		fib:=0;
+	end if;
+	loop
+		exit when counter =n;
+		counter := counter +1;
+		select j, (i+j) into i,j;	
+	end loop;
+	fib:=i;
+	raise notice '%', fib;
+	
+END $$;
+
+
+-- ************ WHILE LOOP *************************
+syntax :
+WHILE condition LOOP
+	statements;
+END LOOP;
+
+
+-- Task : 1 dan 4 e kadar counter değerlerini ekrana basalım
+
+DO $$
+DECLARE
+	n integer :=4;
+	counter integer :=0;
+
+BEGIN
+	WHILE counter<n LOOP
+		counter:=counter+1;
+		raise notice '%', counter;
+	END LOOP;
+
+END $$;
+
+-- Cevap 2:
+
+DO $$
+
+DECLARE
+	counter integer :=0;
+
+BEGIN 
+	WHILE counter<4 LOOP
+		counter := counter+1;
+		raise notice '%', counter;
+	END LOOP;	
+
+END $$ ;
+
+
+
+
+	
+
+
+
+
+
 
